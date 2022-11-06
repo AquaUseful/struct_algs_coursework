d0019c610fc2d1882fd121498451ecf7fb86f349
AquaUseful <l.vladik.m@gmail.com>
2022-11-06 23:31:21 +0300

    feat: dynamic tree order

--- btree/include/BTree.hpp
+++ btree/include/BTree.hpp
@@ -1,19 +1,22 @@
 #pragma once
 
+#include <algorithm>
 #include <concepts>
 #include <memory>
 
 #include "BTreeNode.hpp"
 
 namespace btree {
-template <std::totally_ordered ValT, std::size_t order> class BTree final {
+template <std::totally_ordered ValT> class BTree final {
 public:
   using value_t = ValT;
 
-  using node_t = BTreeNode<value_t, order>;
+  using node_t = BTreeNode<value_t>;
   using node_ptr_t = std::unique_ptr<node_t>;
 
-  BTree() : m_root{nullptr} {}
+  using order_t = typename node_t::size_t;
+
+  BTree(order_t order) : m_order{order}, m_root{nullptr} {}
   BTree(const BTree &) = default;
   BTree(BTree &&) noexcept = default;
   ~BTree() = default;
@@ -23,9 +26,9 @@ public:
 
   void insert(value_t val) {
     if (m_root == nullptr) {
-      m_root = std::make_unique<node_t>();
+      m_root = std::make_unique<node_t>(m_order);
     }
-    m_root->insert(val);
+    root_splitting_insert(val);
   }
 
   bool search(value_t val) {
@@ -35,7 +38,20 @@ public:
     return m_root->search(val);
   }
 
+  void remove(value_t val) {}
+
 private:
+  void root_splitting_insert(value_t val) {
+    typename node_t::SplitResult split_res = m_root->insert(val);
+    if (split_res.sibling != nullptr) {
+      auto new_root =
+          std::make_unique<node_t>(m_order, split_res.median, std::move(m_root),
+                                   std::move(split_res.sibling));
+      m_root = std::move(new_root);
+    }
+  }
+
   node_ptr_t m_root;
+  order_t m_order;
 };
 } // namespace btree
--- btree/include/BTreeNode.hpp
+++ btree/include/BTreeNode.hpp
@@ -1,50 +1,73 @@
 #pragma once
 
 #include <algorithm>
-#include <array>
 #include <concepts>
 #include <cstddef>
 #include <iterator>
 #include <memory>
 
-namespace btree {
-template <std::totally_ordered ValT, std::size_t order> class BTreeNode final {
-  static_assert(order > 1, "Order must be more than 1");
+#include "BTreeNode.hpp"
 
+namespace btree {
+template <std::totally_ordered ValT> class BTreeNode final {
 public:
   using value_t = ValT;
   using value_reference_t = value_t &;
   using value_const_reference_t = const value_t &;
-  using value_array_t = std::array<value_t, order>;
-  using value_iterator_t = typename value_array_t::iterator;
-  using value_const_iterator_t = typename value_array_t::const_iterator;
-  using value_array_size_t = typename value_array_t::size_type;
+  using value_ptr_t = value_t *;
+  using value_iterator_t = value_ptr_t;
+  using value_const_iterator_t = const value_ptr_t;
+  using value_difference_t =
+      decltype(std::distance(value_iterator_t{}, value_iterator_t{}));
+  using value_array_t = value_t[];
+  using value_array_ptr_t = std::unique_ptr<value_array_t>;
 
-  using child_t = BTreeNode<value_t, order>;
+  using child_t = BTreeNode<value_t>;
   using child_ptr_t = std::unique_ptr<child_t>;
-  using child_ptr_array_t = std::array<child_ptr_t, order + 1>;
-  using child_ptr_iterator_t = typename child_ptr_array_t::iterator;
-  using child_ptr_const_iterator_t = typename child_ptr_array_t::const_iterator;
+  using child_ptr_array_t = child_ptr_t[];
+  using child_ptr_array_ptr_t = std::unique_ptr<child_ptr_array_t>;
+  using child_ptr_ptr_t = child_ptr_t *;
+  using child_ptr_iterator_t = child_ptr_ptr_t;
+  using child_ptr_const_iterator_t = const child_ptr_ptr_t;
+  using child_ptr_difference_t =
+      decltype(std::distance(child_ptr_iterator_t{}, child_ptr_iterator_t{}));
 
-  using parent_t = BTreeNode<value_t, order>;
+  using parent_t = BTreeNode<value_t>;
+  using parent_reference_t = parent_t &;
+  using parent_const_reference_r = const parent_t &;
   using parent_ptr_t = parent_t *;
 
-public:
-  BTreeNode() : m_value_count(0), m_leaf(true), m_parent(nullptr) {}
+  using size_t = std::size_t;
 
-  void insert(value_t val) {
-    if (m_leaf) {
-      unbalanced_insert(val);
-      balance();
-    } else {
-      auto insert_pos = std::upper_bound(begin(), end(), val);
-      auto lc = left_child(insert_pos);
-      (*lc)->insert(val);
-    }
+  struct SplitResult {
+    value_t median;
+    child_ptr_t sibling;
+  };
+
+public:
+  BTreeNode(size_t max_size)
+      : m_values{std::make_unique<value_array_t>(max_size)},
+        m_children{std::make_unique<child_ptr_array_t>(max_size + 2)},
+        m_size{0}, m_max_size{max_size}, m_leaf{true} {};
+
+  BTreeNode(size_t max_size, value_t median, child_ptr_t left,
+            child_ptr_t right)
+      : BTreeNode(max_size) {
+    (*begin()) = median;
+    (*left_child(begin())) = std::move(left);
+    (*right_child(begin())) = std::move(right);
+    m_size = 1;
+    m_leaf = false;
   }
 
+  ~BTreeNode() = default;
+
+  SplitResult insert(value_t val) { return std::move(splitting_insert(val)); }
+
+  void remove(value_t val) {}
+
   bool search(value_t val) {
-    auto candidate = std::lower_bound(begin(), end(), val);
+    auto candidate = lower_bound(val);
     if (*candidate != val) {
       if (!m_leaf) {
         return (*left_child(candidate))->search(val);
@@ -54,100 +77,97 @@ public:
     return true;
   }
 
-private:
-  value_iterator_t begin() { return m_values.begin(); }
-  value_iterator_t end() { return begin() + m_value_count; }
+  value_iterator_t begin() { return m_values.get(); }
+  value_iterator_t end() { return begin() + m_size; }
 
   child_ptr_iterator_t left_child(value_iterator_t it) {
-    return m_children.begin() + std::distance(begin(), it);
+    return m_children.get() + (it - m_values.get());
   }
   child_ptr_iterator_t right_child(value_iterator_t it) {
-    return m_children.begin() + std::distance(begin(), it) + 1;
+    return m_children.get() + (it - m_values.get()) + 1;
   }
 
-  value_iterator_t median() { return begin() + (m_value_count / 2); }
+private:
+  value_iterator_t median() {
+    return begin() + (std::distance(begin(), end()) / 2);
+  }
 
-  value_iterator_t unbalanced_insert(value_t val) {
-    auto insert_pos = std::upper_bound(begin(), end(), val);
-    std::shift_right(insert_pos, end() + 1, 1);
+  value_iterator_t upper_bound(value_t val) {
+    return std::upper_bound(begin(), end(), val);
+  }
+
+  value_iterator_t lower_bound(value_t val) {
+    return std::lower_bound(begin(), end(), val);
+  }
+
+  void raw_insert(value_iterator_t pos, value_t val) {
+    std::shift_right(pos, end(), 1);
     if (!m_leaf) {
-      std::shift_right(right_child(insert_pos), right_child(end()) + 1, 1);
+      std::shift_right(right_child(pos), left_child(end()), 1);
     }
-    *insert_pos = val;
-    ++m_value_count;
-    return insert_pos;
+    *pos = val;
+    ++m_size;
   }
 
-  void balance() {
-    if (m_value_count == order) {
-      split();
+  value_iterator_t raw_insert(value_t val) { raw_insert(upper_bound(val)); }
+
+  SplitResult splitting_insert(value_t val) {
+    value_iterator_t ipos = upper_bound(val);
+    if (m_leaf) {
+      raw_insert(ipos, val);
+    } else {
+      child_ptr_iterator_t lc = left_child(ipos);
+      SplitResult insert_res = (*lc)->splitting_insert(val);
+      if (insert_res.sibling != nullptr) {
+        raw_insert(ipos, insert_res.median);
+        child_ptr_iterator_t rc = right_child(ipos);
+        (*rc) = std::move(insert_res.sibling);
+      }
     }
+    if (max_filled()) {
+      return split();
+    }
+    return SplitResult(0, nullptr);
   }
 
-  void split() {
-    if (m_parent == nullptr) {
-      split_root();
-      return;
-    }
-    child_ptr_t right_sibling = std::make_unique<child_t>();
+  SplitResult split() {
+    child_ptr_t right_sibling = std::make_unique<child_t>(m_max_size);
 
-    right_sibling->m_parent = m_parent;
-
+    right_sibling->m_leaf = m_leaf;
     right_sibling->move_values(median() + 1, end());
-
     if (!m_leaf) {
       right_sibling->move_children(right_child(median()),
                                    right_child(end()) + 1);
     }
 
-    value_iterator_t ins_point = m_parent->unbalanced_insert(*median());
-    *(m_parent->right_child(ins_point)) = std::move(right_sibling);
+    SplitResult result{*median(), std::move(right_sibling)};
 
-    m_value_count = std::distance(begin(), median());
+    m_size = std::distance(begin(), median());
 
-    m_parent->balance();
+    return result;
   }
-  void split_root() {
-    child_ptr_t new_left = std::make_unique<child_t>();
-    child_ptr_t new_right = std::make_unique<child_t>();
 
-    new_left->m_parent = this;
-    new_right->m_parent = this;
+  void rotate_right(value_iterator_t pos) {}
 
-    new_left->move_values(begin(), median());
-    new_right->move_values(median() + 1, end());
+  void rotate_left(value_iterator_t pos) {}
 
-    if (!m_leaf) {
-      new_left->move_children(left_child(begin()), right_child(median()));
-      new_right->move_children(right_child(median()), right_child(end()) + 1);
-      new_left->m_leaf = false;
-      new_right->m_leaf = false;
-    }
-
-    *begin() = *median();
-    m_value_count = 1;
-    m_leaf = false;
-
-    *left_child(begin()) = std::move(new_left);
-    *right_child(begin()) = std::move(new_right);
-  }
+  bool min_filled() const { return m_size == (m_max_size / 2); }
+  bool max_filled() const { return m_size >= m_max_size; }
 
   void move_values(value_iterator_t first, value_iterator_t last) {
-    std::move(first, last, m_values.begin());
-    m_value_count = std::distance(first, last);
+    std::move(first, last, m_values.get());
+    m_size = std::distance(first, last);
   }
 
   void move_children(child_ptr_iterator_t first, child_ptr_iterator_t last) {
-    std::move(first, last, m_children.begin());
+    std::move(first, last, m_children.get());
   }
 
 private:
-  value_array_t m_values;
-  child_ptr_array_t m_children;
-  parent_ptr_t m_parent;
-
-  value_array_size_t m_value_count;
-
+  value_array_ptr_t m_values;
+  child_ptr_array_ptr_t m_children;
+  size_t m_size;
+  const size_t m_max_size;
   bool m_leaf;
 };
 } // namespace btree
